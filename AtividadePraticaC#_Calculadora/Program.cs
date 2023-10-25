de4a8512dd07d94152a52720ea455682d020a75f
cucarrox <brunocucarrox@gmail.com>
2023-10-25 20:47:51 -0300

    Alterra - Calculadora (Inicio do Projeto)

---
+++ AtividadePraticaC#_Calculadora/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AtividadePraticaC__Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
