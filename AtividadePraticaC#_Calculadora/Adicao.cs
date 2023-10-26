eb62c379d50b150828d349edf8946ea3d96478b5
cucarrox <brunocucarrox@gmail.com>
2023-10-25 21:35:15 -0300

    Alterra - Calculadora (main)

---
+++ AtividadePraticaC#_Calculadora/Adicao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadePraticaC__Calculadora
+{
+    internal class Adicao
+    {
+        public double CalcularSoma(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
---
+++ AtividadePraticaC#_Calculadora/Divisao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadePraticaC__Calculadora
+{
+    internal class Divisao
+    {
+
+        public double CalcularDivisao(double n1, double n2)
+        {
+            if (n2 != 0)
+                return n1 / n2;
+            else
+            {
+                Console.WriteLine("Erro: Divisão por zero!");
+                return 0;
+            }
+        }
+    }
+}
